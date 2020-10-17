9df7dd62c5f94663fdd332a4c94ee5d02474d35f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-17 10:19:54 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int p1, p2, k;
+		cin >> p1 >> p2 >> k;
+		int turn = (p1 + p2) / k;
+		cout << (turn % 2 == 0 ? "CHEF" : "COOK") << '\n';
+	}
+	return 0;
+}
